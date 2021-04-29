06038eff1efa8b5d34cd2df98c3562c77cd74462
kudelkod <55825143+kudelkod@users.noreply.github.com>
2021-04-30 01:30:57 +0300

    Study Models

--- micro_blog/test_blog/admin.py
+++ micro_blog/test_blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Example)
+admin.site.register(models.Book)
+admin.site.register(models.Publication)
+admin.site.register(models.Article)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    # list_display = ['name', 'surname']
+    # exclude = ['name']
+    # fields = ['surname']
+    list_display = [field.name for field in models.Author._meta.fields]
+    list_filter = ['name']
+    # search_fields = ['name']
+    search_fields = [field.name for field in models.Author._meta.fields]
+
+    class Meta:
+        model = models.Author
+
+
+admin.site.register(models.Author, AuthorAdmin)
--- micro_blog/test_blog/models.py
+++ micro_blog/test_blog/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
+
 # Create your models here.
+class Example(models.Model):
+    integer_field = models.IntegerField()
+    positive_field = models.PositiveIntegerField()
+    positive_small_field = models.PositiveSmallIntegerField()
+    big_integer_field = models.BigIntegerField()
+    float_field = models.FloatField()
+    email = models.EmailField()
+    text_field = models.TextField(max_length=20)
+    char_field = models.CharField(max_length=10)
+    boolean_field = models.BooleanField(default=False)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=25, verbose_name="Имя")
+    surname = models.CharField(max_length=25, verbose_name="Фамилия")
+    date_birth = models.DateField(auto_now=False, verbose_name="Дата рождения")
+
+
+    def __str__(self):
+        return "Имя: %s, Фамилия: %s", (self.name, self.surname)
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+
+class Book(models.Model):
+    CHOICE_GENRE = (
+        ('comedy', 'Comedy'),
+        ('tragedy', 'Tragedy'),
+        ('drama', 'Drama'),
+    )
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=1000)
+    genre = models.CharField(max_length=50, choices=CHOICE_GENRE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Книга"
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Article(models.Model):
+    publications = models.ManyToManyField(Publication)
+    headline = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
